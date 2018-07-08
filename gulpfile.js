1c661ef83293ec4327e6838619e626b14cce849a
139199228 <139199228@qq.com>
2018-07-08 16:34:14 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const babel = require("gulp-babel")
 // gulp.task("default",['praise'],()=>{
 //     gulp.watch(['src/**/*.es6','!src/public/**/*.es6'],['praise'])
 // })
-gulp.task('praise',()=>{
+gulp.task('default',()=>{
     gulp.src(['src/**/*.es6','!src/public/**/*.es6'])
         .pipe(babel({
             presets:['env']
